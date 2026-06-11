26056ff86993fd4c6a8ec932514b26da0fc3ab11
rishirana463-hub <rishirana463@gmail.com>
2026-06-11 22:38:20 +0530

    yokoso

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import FruitsSearch from './FruitsSearch';
+import './App.css';
+function App() {
+  return (
+    <div>
+      <h1>Fruit Search</h1>
+      <FruitsSearch />
+    </div>
+  );
+}
+
+export default App;
